3f0677145b0f22b20dc570c7e05ba45d4870cd9f
Aled James <aledjms@gmail.com>
2018-05-29 19:52:38 +0100

    Improve terminal output

--- example/main.go
+++ example/main.go
@@ -15,23 +15,23 @@ func populaterequestparameters(apikey string) wb.Parameters {
 	P := wb.Parameters{}
 
 	// Steam Crane Pub, Bristol
-	P.City = "" // EITHER specify city OR specify lat & lon
+	// P.City = "Johannesburg" // EITHER specify city OR specify lat & lon
 	P.Lat = 51.4415
 	P.Lon = -2.6017
-	P.Temporality = "forecast" // "current", "history", "forecast"
+	P.Temporality = "history" // "current", "history", "forecast"
 	P.Apikey = apikey
 
 	// FOR USE WITH HISTORY OR FORECAST QUERIES
-	P.Granularity = "hourly"      // specify "hourly" OR "daily"
-	P.StartDate = "2018-05-19:14" // [YYYY-MM-DD OR YYYY-MM-DD:HH]
-	P.EndDate = "2018-05-20:02"   // [YYYY-MM-DD OR YYYY-MM-DD:HH]
+	P.Granularity = "daily"    // specify "hourly" OR "daily"
+	P.StartDate = "2018-05-20" // [YYYY-MM-DD OR YYYY-MM-DD:HH]
+	P.EndDate = "2018-05-23"   // [YYYY-MM-DD OR YYYY-MM-DD:HH]
 
 	return P
 }
 
 func main() {
 
-	fmt.Println("Starting")
+	fmt.Println("Open browser to http://0.0.0.0:8080")
 	http.HandleFunc("/", drawChart)
 	http.ListenAndServe(":8080", nil)
 
