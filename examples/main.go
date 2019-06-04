c17555c61923a3a3b41d047e45b735d7eafc1a5a
Zee <zeieshanahmed@icloud.com>
2019-06-04 11:50:13 +0100

    Wrapping QueryEscape around City and Country

--- examples/main.go
+++ examples/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	wb "github.com/alljames/weatherbit-go/pkg"
 	"os"
+	"net/url"
 )
 
 func main() {
@@ -16,8 +17,8 @@ func main() {
 	// holds easily customisable fields to make an API request
 	// The weatherbit-go package takes care of the rest!
 	params := wb.Parameters{
-		Lat:         51.4415, // Steam Crane Pub, Bristol
-		Lon:         -2.6017,
+		City: url.QueryEscape("New York"),
+		Country: url.QueryEscape("United States"),
 		Temporality: "history", // "current", "history", "forecast"
 		Apikey:      os.Getenv("WBITKEY"),
 
@@ -25,8 +26,8 @@ func main() {
 		Granularity: "hourly",
 
 		// required only for "history" queries
-		StartDate: "2019-04-08", // [YYYY-MM-DD OR YYYY-MM-DD:HH]
-		EndDate:   "2019-04-09", // [YYYY-MM-DD OR YYYY-MM-DD:HH]
+		StartDate: "2019-06-04", // [YYYY-MM-DD OR YYYY-MM-DD:HH]
+		EndDate:   "2019-06-05", // [YYYY-MM-DD OR YYYY-MM-DD:HH]
 	}
 
 	fmt.Println(wb.GetResponse(params))
